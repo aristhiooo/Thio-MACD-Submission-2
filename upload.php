00dfb31e25d6c3b8f3107f20020cc03056fab445
ARISTIYO RAHADIAN AKBAR <44568681+aristhiooo@users.noreply.github.com>
2019-05-14 15:00:20 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
 # Mengatur instance dari Azure::Storage::Client
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=thiowebapps;AccountKey=qz3LFc/8O885IctHD74a/zfurR1PcFofvo0+ap+8elmeJj2POb1bc9vvUlo7j5VV1kap4hE+C+uZqO5SMZ3g3g==;EndpointSuffix=core.windows.net";
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=thiowebapps;AccountKey=qz3LFc/8O885IctHD74a/zfurR1PcFofvo0+ap+8elmeJj2POb1bc9vvUlo7j5VV1kap4hE+C+uZqO5SMZ3g3g==;EndpointSuffix=core.windows.net;
 
 // Membuat blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
