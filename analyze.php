ebddb7da820bb8db37bd58906a822e6fc5022208
ARISTIYO RAHADIAN AKBAR <44568681+aristhiooo@users.noreply.github.com>
2019-05-18 01:28:08 +0800

    Update analyze.php

--- analyze.php
+++ analyze.php
@@ -27,13 +27,13 @@ if (isset($_POST['submit'])) {
 	</head>
 	
 	<body>
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
+					<li class="nav-item">
 						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/">Submission 1</a>
 					</li>
 					<li class="nav-item">
