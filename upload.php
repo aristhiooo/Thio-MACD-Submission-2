f236af9fd19311d342379c1d4c77e5eb1f6b8454
ARISTIYO RAHADIAN AKBAR <44568681+aristhiooo@users.noreply.github.com>
2019-05-18 01:25:03 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -43,17 +43,17 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 	</head>
 	
 	<body>
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top justify-content-center">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top justify-content-center">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarsExampleDefault">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item active">
-						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/">Submission 1</a>
+						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/">Submission 1<span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/upload.php">Submission 2<span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/upload.php">Submission 2</a>
 					</li>
 					</div>
 				</nav>
