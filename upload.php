cef043de5d08887e6d93dd38bb6d5097c558c33b
ARISTIYO RAHADIAN AKBAR <44568681+aristhiooo@users.noreply.github.com>
2019-05-18 00:15:29 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -30,66 +30,62 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 <!DOCTYPE html>
 <html>
 	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Formulir Calon Anggota UKM Seni UNM</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
-  </head>
-  
-<body>
-	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="https://thio-webapps.azurewebsites.net/">Home</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="https://thio-webapps.azurewebsites.net/upload.php">Upload dan Analisis Gambar.<span class="sr-only">(current)</span></a>
-			</li>
-		</div>
-		</nav>
-		
-		<main role="main" class="container">
-    		<div class="starter-template"> <br><br><br>
-        		<h1>Analisis Alat Musik</h1>
-				<p class="lead">Pilih foto alat musik anda<br> Kemudian klik <b>Upload</b>. Untuk menganlisa foto, pilih <b>analyze</b> pada tabel.</p>
-				<span class="border-top my-3"></span>
-		</div>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>Formulir Calon Anggota UKM Seni UNM</title>
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+		<!-- Bootstrap core CSS -->
+		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="starter-template.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/">Submission 1</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="https://thio-webapps.azurewebsites.net/upload.php">Submission 2<span class="sr-only">(current)</span></a>
+					</li>
+					</div>
+				</nav>
 			
-		<div class="mt-4 mb-2">
-			<form class="d-flex justify-content-lefr" action="upload.php" method="post" enctype="multipart/form-data">
-				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
-				<input type="submit" name="submit" value="Upload">
-			</form>
-		</div>
-		<br>		
-		<table class='table table-hover'>
-			<thead>
-				<tr>
-					<th>File Name</th>
-					<th>File URL</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				do {
-					foreach ($result->getBlobs() as $blob)
-					{
-						?>
+			<main role="main" class="container">
+				<div class="starter-template"> <br><br><br>
+					<h1>Analisis Alat Musik</h1>
+					<p class="lead">Pilih foto alat musik anda<br> Kemudian klik <b>Upload</b>. Untuk menganlisa foto, pilih <b>analyze</b> pada tabel.</p>
+					<span class="border-top my-3"></span>
+				</div>				
+				<div class="mt-4 mb-2">
+					<form class="d-flex justify-content-lefr" action="upload.php" method="post" enctype="multipart/form-data">
+						<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+						<input type="submit" name="submit" value="Upload">
+					</form>
+				</div>
+				<br>
+				
+				<table class='table table-hover'>
+					<thead>
+						<tr>
+							<th>File Name</th>
+							<th>File URL</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					
+					<tbody>
+						<?php
+						do {
+							foreach ($result->getBlobs() as $blob) {
+						?>						
 						<tr>
 							<td><?php echo $blob->getName() ?></td>
 							<td><?php echo $blob->getUrl() ?></td>
@@ -101,19 +97,18 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 							</td>
 						</tr>
 						<?php
-					}
-					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
-				} while($result->getContinuationToken());
-				?>
-			</tbody>
-		</table>
-
-	</div>
-
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
-    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
- </body>
-</html>
+							} $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+						} while($result->getContinuationToken());
+						?>
+					</tbody>
+				</table>
+				</div>
+			
+			<!-- Placed at the end of the document so the pages load faster -->
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+			<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+			<script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+			
+			</body>
+		</html>
